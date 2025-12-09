7a8196e61893b5f61a5054b350eb69183d82a2c6
Olya Chistikova <olya.chistikova@mail.ru>
2025-12-09 18:44:07 +0300

    Added BaseTest

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import helpers.JavaScriptExecutorHelper;
+import io.qameta.allure.Step;
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+
+@Getter
+public class BaseTest {
+    private WebDriver driver;
+    protected boolean useIncognito = false;
+    protected SoftAssert softAssert;
+
+    @BeforeMethod(description = "Настройка браузера перед запуском тестов")
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        softAssert = new SoftAssert();
+    }
+
+    @Step("Делаем скриншот в случае провала теста")
+    public void takeScreenshotOnFailure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            JavaScriptExecutorHelper.takeScreenshot(driver);
+        }
+    }
+
+    @AfterMethod(description = "Закрываем браузер")
+    public void tearDown(ITestResult result){
+        takeScreenshotOnFailure(result);
+        if (driver != null){
+            driver.quit();
+        }
+    }
+}
